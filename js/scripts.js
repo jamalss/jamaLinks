77a6fcd545871d4c5c9edbee52944f8c23f68885
jamalss <jamal8010@gmail.com>
2023-08-06 13:45:12 -0400

    add .js files to js repo

---
+++ js/scripts.js
@@ -0,0 +1,86 @@
+$(document).ready(function() {
+	
+	$(".clicker").click(function() {
+		$(".shown").fadeToggle(1000, function() {
+			$(".hidden").fadeToggle(1000);
+		});
+	});
+	
+	$(".clicker1").click(function() {
+		$(".shown1").fadeToggle(1000, function() {
+			$(".hidden1").fadeToggle(1000);
+		});
+	});
+	
+		$(".clicker2").click(function() {
+		$(".shown2").fadeToggle(1000, function() {
+			$(".hidden2").fadeToggle(1000);
+		});
+	});
+	
+		$(".clicker3").click(function() {
+		$(".shown3").fadeToggle(1000, function() {
+			$(".hidden3").fadeToggle(1000);
+		});
+	});
+	
+		$(".clicker4").click(function() {
+		$(".shown4").fadeToggle(1000, function() {
+			$(".hidden4").fadeToggle(1000);
+		});
+	});
+	
+		$(".clicker5").click(function() {
+		$(".shown5").fadeToggle(1000, function() {
+			$(".hidden").fadeToggle(1000);
+		});
+	});
+	
+		$(".clicker6").click(function() {
+		$(".shown6").fadeToggle(1000, function() {
+			$(".hidden").fadeToggle(1000);
+		});
+	});
+	
+		$(".clicker7").click(function() {
+		$(".shown7").fadeToggle(1000, function() {
+			$(".hidden").fadeToggle(1000);
+		});
+	});
+	
+		$(".clicker8").click(function() {
+		$(".shown8").fadeToggle(1000, function() {
+			$(".hidden8").fadeToggle(1000);
+		});
+	});
+	
+		$(".clicker9").click(function() {
+		$(".shown9").fadeToggle(1000, function() {
+			$(".hidden9").fadeToggle(1000);
+		});
+	});
+	
+		$(".clicker10").click(function() {
+		$(".shown10").fadeToggle(1000, function() {
+			$(".hidden10").fadeToggle(1000);
+		});
+	});
+	
+		$(".clicker11").click(function() {
+		$(".shown11").fadeToggle(1000, function() {
+			$(".hidden11").fadeToggle(1000);
+		});
+	});
+	
+		$(".clicker12").click(function() {
+		$(".shown12").fadeToggle(1000, function() {
+			$(".hidden12").fadeToggle(1000);
+		});
+	});
+	
+	
+	
+	
+	
+});
+
